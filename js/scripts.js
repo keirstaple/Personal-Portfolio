574fefbfdf0da775317d0a670131aa540c4ea132
Keir Staple <keirstaple@comcast.net>
2016-07-25 09:59:27 -0700

    fix spacing issue with mobile, but now need to fix my picture spacing in larger views and bug with the links not appearing when you move screen between sizes after a click

--- js/scripts.js
+++ js/scripts.js
@@ -11,8 +11,8 @@ $(document).ready(function(){
   });
   //Toggle the menu icon when in mobile view
   $('.fa-bars').on('click', function() {
-    console.log('clicked');
+    // console.log('clicked');
 
-    $('.nav-links').toggle(200);
+    $('.nav-links').toggle();
   });
 });
